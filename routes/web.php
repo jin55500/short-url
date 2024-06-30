5a4a891d83be7869a964533337f0065cc261271f
jin55500 <99634112+jin55500@users.noreply.github.com>
2024-06-30 13:38:55 +0700

    d

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('test');
+    // return view('home');
 })->name('home')->middleware('auth');
 
 Route::group(['prefix' => 'auth'], function () {
